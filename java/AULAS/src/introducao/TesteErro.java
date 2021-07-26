8c3ef81cbab1db6152950f707efa96e395d6f8db
LUANwesDEV <luanwesley.wll@gmail.com>
2021-07-26 12:11:27 -0300

    Primeira aplicacao Spring Boot

--- java/AULAS/src/introducao/TesteErro.java
+++ java/AULAS/src/introducao/TesteErro.java
@@ -1,11 +1,11 @@
-package introducao;
+/*package introducao;
 
 import java.util.Scanner;
 
 public class TesteErro {
 		Scanner leia = new Scanner(System.in);
 		int valor=0;
-		String nome[]=new String[3];
+		String nome[]=new String[3] {
 		
 		try {
 			System.out.println("Digite a posição do vetor de 0 a 2 para cadastro do nome: ");
@@ -16,5 +16,6 @@ public class TesteErro {
 		catch (java.lang.ArrayIndexOutOfBoundsException erro) {
 			System.out.println("Você colocou um tamanho errado ou posição incorreta da matriz");
 		}
-		
+  }	
 }
+*/
