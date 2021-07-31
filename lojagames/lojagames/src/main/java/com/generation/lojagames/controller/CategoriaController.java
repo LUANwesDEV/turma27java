7a638e931b7c0b333fd26ff2b86760541a84c012
LUANwesDEV <luanwesley.wll@gmail.com>
2021-07-30 23:35:36 -0300

    Atividade Back end loja de games

---
+++ lojagames/lojagames/src/main/java/com/generation/lojagames/controller/CategoriaController.java
@@ -0,0 +1,31 @@
+package com.generation.lojagames.controller;
+
+import java.lang.ModuleLayer.Controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.lojagames.model.Categoria;
+import com.generation.lojagames.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping
+@CrossOrigin("*")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	
+
+}
